ccc75cf893b390bed3ed80486671ecd59cf896e5
nijarbelajar <belajarnijar@gmail.com>
2026-05-18 09:37:39 +0700

    Add files via upload

---
+++ game_masak.py
@@ -0,0 +1,16 @@
+bumbu = []
+bahan = []
+alat = []
+menu = []
+
+while True :
+    pilihan =input ("Mau masak apa hari ini ?\n atau belum ada ide memasak: ")
+    if pilihan.lower == "ya" :
+        if (len.menu) == 0 :
+            print ("Belum ada menu,silahkan buat menu baru")
+    else : 
+        menu.append(pilihan)
+        print ("Menu Berhasil ditambahkan")
+    break
+        
+    
